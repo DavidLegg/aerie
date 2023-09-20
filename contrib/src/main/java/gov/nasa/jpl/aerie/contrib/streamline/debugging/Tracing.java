616c6fb1442b36ea71e574d28c6a38f8cea980a8
David Legg <david.legg@jpl.nasa.gov>
2023-09-20 13:56:27 -0700

    WIP: Add tracing for Conditions

--- contrib/src/main/java/gov/nasa/jpl/aerie/contrib/streamline/debugging/Tracing.java
+++ contrib/src/main/java/gov/nasa/jpl/aerie/contrib/streamline/debugging/Tracing.java
@@ -3,10 +3,12 @@ package gov.nasa.jpl.aerie.contrib.streamline.debugging;
 import gov.nasa.jpl.aerie.contrib.streamline.core.ErrorCatching;
 import gov.nasa.jpl.aerie.contrib.streamline.core.Expiring;
 import gov.nasa.jpl.aerie.contrib.streamline.core.Resource;
+import gov.nasa.jpl.aerie.merlin.framework.Condition;
 import gov.nasa.jpl.aerie.merlin.protocol.types.Unit;
 
 import java.util.Stack;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 import static gov.nasa.jpl.aerie.contrib.streamline.core.ErrorCatching.failure;
 import static gov.nasa.jpl.aerie.contrib.streamline.core.Resources.currentTime;
@@ -17,7 +19,7 @@ import static gov.nasa.jpl.aerie.contrib.streamline.core.Resources.currentTime;
 public final class Tracing {
   private Tracing() {}
 
-  private final static Stack<String> activeResources = new Stack<>();
+  private final static Stack<String> activeTracePoints = new Stack<>();
 
   public static <D> Resource<D> trace(String name, Resource<D> resource) {
     return traceFull(name, $ -> {}, resource);
@@ -37,22 +39,37 @@ public final class Tracing {
   }
 
   public static <D> Resource<D> traceFull(String name, Consumer<ErrorCatching<Expiring<D>>> assertion, Resource<D> resource) {
-    return () -> {
-      activeResources.push(name);
-      System.out.printf("TRACE: %s - %s computing...", currentTime(), formatStack());
+    return () -> traceAction(name, () -> {
       var result = resource.getDynamics();
       try {
         assertion.accept(result);
       } catch (Exception e) {
         result = failure(e);
       }
-      System.out.printf("TRACE: %s - %s: %s%n", currentTime(), formatStack(), result);
-      activeResources.pop();
       return result;
-    };
+    });
+  }
+
+  public static Condition trace(String name, Condition condition) {
+    return (positive, atEarliest, atLatest) ->
+        traceAction(name, () -> condition.nextSatisfied(positive, atEarliest, atLatest));
+  }
+
+  public static Supplier<Condition> trace(String name, Supplier<Condition> condition) {
+    // Trace calling the supplier separately from tracing the condition itself.
+    return () -> traceAction(name + " (generation)", () -> trace(name, condition.get()));
+  }
+
+  private static <T> T traceAction(String name, Supplier<T> action) {
+    activeTracePoints.push(name);
+    System.out.printf("TRACE: %s - %s start...", currentTime(), formatStack());
+    T result = action.get();
+    System.out.printf("TRACE: %s - %s: %s%n", currentTime(), formatStack(), result);
+    activeTracePoints.pop();
+    return result;
   }
 
   private static String formatStack() {
-    return String.join("->", activeResources);
+    return String.join("->", activeTracePoints);
   }
 }
