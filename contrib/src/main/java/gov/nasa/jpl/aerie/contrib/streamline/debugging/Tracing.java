da3944d187843d9dedce073cf90f14e9cf64c39f
David Legg <david.legg@jpl.nasa.gov>
2023-09-20 12:44:30 -0700

    WIP: Add tracing helper functions

---
+++ contrib/src/main/java/gov/nasa/jpl/aerie/contrib/streamline/debugging/Tracing.java
@@ -0,0 +1,62 @@
+package gov.nasa.jpl.aerie.contrib.streamline.debugging;
+
+import gov.nasa.jpl.aerie.contrib.streamline.core.ErrorCatching;
+import gov.nasa.jpl.aerie.contrib.streamline.core.Expiring;
+import gov.nasa.jpl.aerie.contrib.streamline.core.Resource;
+import gov.nasa.jpl.aerie.contrib.streamline.core.monads.DynamicsMonad;
+import gov.nasa.jpl.aerie.contrib.streamline.core.monads.ErrorCatchingToResourceMonad;
+import gov.nasa.jpl.aerie.contrib.streamline.core.monads.ResourceMonad;
+import gov.nasa.jpl.aerie.merlin.protocol.types.Unit;
+
+import java.util.Stack;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import static gov.nasa.jpl.aerie.contrib.streamline.core.ErrorCatching.failure;
+import static gov.nasa.jpl.aerie.merlin.protocol.types.Unit.UNIT;
+
+/**
+ * Functions for debugging resources by tracing their calculation.
+ */
+public final class Tracing {
+  private Tracing() {}
+
+  private final static Stack<String> activeResources = new Stack<>();
+
+  public static <D> Resource<D> trace(String name, Resource<D> resource) {
+    return traceFull(name, $ -> {}, resource);
+  }
+
+  public static <D> Resource<D> trace(String name, Consumer<D> assertion, Resource<D> resource) {
+    return traceExpiring(name, $ -> assertion.accept($.data()), resource);
+  }
+
+  public static <D> Resource<D> traceExpiring(String name, Consumer<Expiring<D>> assertion, Resource<D> resource) {
+    return traceFull(name, $ -> $.match(d -> {
+      assertion.accept(d);
+      return Unit.UNIT;
+    }, e -> {
+      throw new AssertionError("%s failed while computing".formatted(formatStack()), e);
+    }), resource);
+  }
+
+  public static <D> Resource<D> traceFull(String name, Consumer<ErrorCatching<Expiring<D>>> assertion, Resource<D> resource) {
+    return () -> {
+      activeResources.push(name);
+      System.out.printf("TRACE: %s computing...", formatStack());
+      var result = resource.getDynamics();
+      try {
+        assertion.accept(result);
+      } catch (Exception e) {
+        result = failure(e);
+      }
+      System.out.printf("TRACE: %s: %s%n", formatStack(), result);
+      activeResources.pop();
+      return result;
+    };
+  }
+
+  private static String formatStack() {
+    return String.join("->", activeResources);
+  }
+}
